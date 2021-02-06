0d8db735b94655466356f6f976a00e792f16acc1
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2021-02-07 03:18:22 +0900

    add curl -L

--- recipes/vscode_liveshare.rb
+++ recipes/vscode_liveshare.rb
@@ -1,7 +1,7 @@
 execute 'install vscode liveshare dependencies' do
   command <<-EOS
     sudo apt-get update -y
-    curl -o ~/vsls-reqs https://aka.ms/vsls-linux-prereq-script
+    curl -Lo ~/vsls-reqs https://aka.ms/vsls-linux-prereq-script
     chmod +x ~/vsls-reqs
     ~/vsls-reqs
   EOS
