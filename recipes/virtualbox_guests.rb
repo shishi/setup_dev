dfeee30cc9983392b6fbeeaf6c81b1722febdfb9
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2021-02-13 13:40:10 +0900

    Revert "add curl -L"
    This reverts commit 0d8db735b94655466356f6f976a00e792f16acc1.

--- recipes/virtualbox_guests.rb
+++ recipes/virtualbox_guests.rb
@@ -1,6 +1,6 @@
 execute 'install virtualbox-guest-x11' do
   command <<-EOS
     sudo apt-get update -y
-    sudo apt-get install -y --no-install-recommends virtualbox-guest-x11
+    DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends virtualbox-guest-x11
   EOS
 end
--- recipes/vscode_liveshare.rb
+++ recipes/vscode_liveshare.rb
@@ -1,6 +1,9 @@
 execute 'install vscode liveshare dependencies' do
   command <<-EOS
-    # https://docs.microsoft.com/ja-jp/visualstudio/liveshare/reference/linux
-    sudo apt install libssl1.0.0 libkrb5-3 zlib1g libicu[0-9][0-9] gnome-keyring libsecret-1-0 desktop-file-utils x11-utils
+    sudo apt-get update -y
+    curl -sSLo ~/vsls-reqs https://aka.ms/vsls-linux-prereq-script
+    chmod +x ~/vsls-reqs
+    ~/vsls-reqs
+    rm ~/vsls-reqs
   EOS
 end
