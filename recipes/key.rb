d9c936c6eb4860119019fa826b734a0c57bd5763
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2021-02-13 19:37:12 +0900

    fix key owner

--- recipes/key.rb
+++ recipes/key.rb
@@ -3,9 +3,12 @@ execute 'copy keys if in WSL' do
   only_if 'test -d /mnt/c/Users/shishi/OneDrive/dev/key'
 end
 
+user = ENV.fetch('USER', 'shishi')
+
 execute 'copy keys if in multipass' do
   command <<-EOS
     sudo cp -fr /home/ubuntu/key ~/
+    sudo chown -R #{user}:#{user} ~/key
   EOS
   only_if 'test -d /home/ubuntu/key'
 end
@@ -13,6 +16,7 @@ end
 execute 'copy keys if in vagrant' do
   command <<-EOS
     sudo cp -fr /synced_home/dev/key ~/
+    sudo chown -R #{user}:#{user} ~/key
   EOS
   only_if 'test -d /synced_home/dev/key'
 end
@@ -20,6 +24,7 @@ end
 execute 'copy keys if in vagrant ver.2' do
   command <<-EOS
     sudo cp -fr /synced_home/OneDrive/dev/key ~/key
+    sudo chown -R #{user}:#{user} ~/key
   EOS
   only_if 'test -d /synced_home/OneDrive/dev/key'
 end
@@ -27,23 +32,20 @@ end
 execute 'copy keys if in normal virtualbox' do
   command <<-EOS
     sudo cp -fr /media/sf_shishi/OneDrive/dev/key/ ~/key
+    sudo chown -R #{user}:#{user} ~/key
   EOS
   only_if 'test -d /media/sf_shishi/OneDrive/dev/key/'
 end
 
-user = ENV.fetch('USER', 'shishi')
-
 execute 'install ssh key' do
   command <<-EOS
     sudo cp -fr ~/key/.ssh ~/
-    sudo chown -R #{user}:#{user} ~/.ssh
     sudo chmod 600 ~/.ssh/id_rsa*
   EOS
 end
 
 execute 'install gpg key' do
   command <<-EOS
-    sudo chown -R #{user}:#{user} ~/key/gpg
     gpg --import ~/key/gpg/shishi.pub.gpg
     gpg --import --allow-secret-key-import ~/key/gpg/shishi.sub.gpg
   EOS
