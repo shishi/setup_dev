c16467d451b06976f0998cc6be1d4a53309e0906
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2021-02-15 02:21:34 +0900

    add hackgen

--- recipes/fonts.sh
+++ recipes/fonts.sh
@@ -7,9 +7,14 @@ mkdir -p ~/.fonts
 curl -fsSLO --retry 3 https://github.com/tomokuni/Myrica/raw/master/product/MyricaM.zip
 unzip -d myricam MyricaM.zip 
 cp myricam/MyricaM.TTC ~/.fonts/
-rm -fr myricam
+rm -fr MyricaM.zip myricam
 
-curl -fsSL --retry 3 https://github.com/edihbrandon/RictyDiminished/archive/3.2.3.zip -o ricty.zip
-unzip ricty.zip
+curl -fsSLO --retry 3 https://github.com/edihbrandon/RictyDiminished/archive/3.2.3.zip
+unzip 3.2.3.zip
 cp RictyDiminished-3.2.3/*.ttf ~/.fonts/
-rm -fr ricty.zip RictyDiminished-3.2.3
+rm -fr 3.2.3.zip RictyDiminished-3.2.3
+
+curl -fsSLO --retry 3 https://github.com/yuru7/HackGen/releases/download/v2.2.3/HackGenNerd_v2.2.3.zip
+unzip HackGenNerd_v2.2.3.zip
+cp HackGenNerd_v2.2.3/*.ttf ~/.fonts/
+rm -fr HackGenNerd_v2.2.3.zip HackGenNerd_v2.2.3
