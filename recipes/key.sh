00197e9a83fd1b0b2784379840dde3ffb9b9a6b6
Shigenobu Nishikawa <shishi@srevo.net>
2021-12-26 23:28:41 +0900

    gpg trust

--- recipes/key.sh
+++ recipes/key.sh
@@ -25,4 +25,5 @@ cp -fr ~/key/.ssh ~/
 sudo chmod 600 ~/.ssh/id_rsa*
 
 gpg --import --allow-secret-key-import ~/key/gpg/shishi.sec_sub.gpg
+gpg --import-ownertrust ~/key/gpg/shishi.ownertrust
 gpg --import --allow-secret-key-import ~/key/gpg/shishisn.sec_sub.gpg
