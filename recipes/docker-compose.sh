ea84c9c79c9de034e4495b20c02dec7ce5bc145b
Shigenobu Nishikawa <shishi@srevo.net>
2022-01-07 21:49:20 +0900

    install docker-compose as plugin

--- recipes/docker-compose.sh
+++ recipes/docker-compose.sh
@@ -2,5 +2,7 @@
 
 set -ex
 
-sudo curl -fsSL --retry 3 https://github.com/docker/compose/releases/latest/download/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+dir="/usr/local/lib/docker/cli-plugins"
+mkdir -p ${dir}
+sudo curl -fsSL --retry 3 https://github.com/docker/compose/releases/latest/download/docker-compose-`uname -s`-`uname -m` -o  ${dir}/docker-compose
+sudo chmod +x {$dir}/docker-compose
