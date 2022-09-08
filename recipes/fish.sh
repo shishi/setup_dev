e279d9f6959fdd7c98f1a55fef479143660d3f5b
Shigenobu Nishikawa <shishi@srevo.net>
2022-09-08 15:58:39 +0900

    alrady defined .bashrc

--- recipes/fish.sh
+++ recipes/fish.sh
@@ -5,7 +5,7 @@ set -ex
 sudo add-apt-repository -y ppa:fish-shell/release-3
 sudo apt-get update -qq
 sudo apt-get install -yqq --no-install-recommends fish
-echo "exec fish" >> ~/.bashrc
+# echo "exec fish" >> ~/.bashrc
 
 # fisher
 # curl -fsSL --retry 3 https://git.io/fisher --create-dirs -sLo  ~/.config/fish/functions/fisher.fish
