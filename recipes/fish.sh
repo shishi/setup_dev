5049d88fa30d60d778c808eab861687a8cbe5fb4
Shigenobu Nishikawa <shishi@srevo.net>
2022-09-21 13:55:57 +0900

    fish_update_completions

--- recipes/fish.sh
+++ recipes/fish.sh
@@ -6,6 +6,7 @@ sudo add-apt-repository -y ppa:fish-shell/release-3
 sudo apt-get update -qq
 sudo apt-get install -yqq --no-install-recommends fish
 # echo "exec fish" >> ~/.bashrc
+fish -c 'fish_update_completions'
 
 # fisher
 # curl -fsSL --retry 3 https://git.io/fisher --create-dirs -sLo  ~/.config/fish/functions/fisher.fish
