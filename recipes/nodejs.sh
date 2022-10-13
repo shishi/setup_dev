01a14a25f4d5de52e9cea8ef66fac11585c1e1fd
Shigenobu Nishikawa <shishi@srevo.net>
2022-10-13 16:47:00 +0900

    add tree-sitter-cli

--- recipes/nodejs.sh
+++ recipes/nodejs.sh
@@ -6,3 +6,4 @@ sudo curl -fsSL --retry 3 https://deb.nodesource.com/setup_lts.x | sudo bash -
 sudo apt-get update -qq
 sudo apt-get install -yqq --no-install-recommends nodejs
 
+npm --prefix ~/.local/ install -g typescript prettier @fsouza/prettierd eslint eslint-config-prettier eslint-plugin-react eslint-plugin-react-hooks eslint_d jsonlint stylelint stylelint-config-standard sql-formatter
--- recipes/rustup.sh
+++ recipes/rustup.sh
@@ -8,4 +8,4 @@ mkdir -p ~/.cargo
 cp ./files/cargo_config ~/.cargo/config
 
 ~/.cargo/bin/rustup component add rust-src rustfmt clippy
-~/.cargo/bin/cargo install carto-edit bat exa fd-find selene stylua taplo-cli
+~/.cargo/bin/cargo install carto-edit bat exa fd-find selene stylua taplo-cli tree-sitter-cli
