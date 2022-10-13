46d4402e453abe4ba9416c8d1217fcadedf84057
Shigenobu Nishikawa <shishi@srevo.net>
2022-10-13 16:47:19 +0900

    rust-analyzer

--- recipes/rustup.sh
+++ recipes/rustup.sh
@@ -7,5 +7,5 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 mkdir -p ~/.cargo
 cp ./files/cargo_config ~/.cargo/config
 
-~/.cargo/bin/rustup component add rust-src rustfmt clippy
+~/.cargo/bin/rustup component add rust-src rust-analyzer rustfmt clippy
 ~/.cargo/bin/cargo install carto-edit bat exa fd-find selene stylua taplo-cli tree-sitter-cli
