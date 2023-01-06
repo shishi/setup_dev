61672b6a1724219a6bd4ed41a34c86516acb35ef
Shigenobu Nishikawa <shishi@srevo.net>
2023-01-06 18:29:10 +0900

    typo

--- recipes/vscode_liveshare.sh
+++ recipes/vscode_liveshare.sh
@@ -3,12 +3,14 @@
 set -ex
 
 sudo apt-get update -qq
+
+# ubuntu 22.04 don't have libssl 1.1
+curl -kfsSL --retry 3 -O http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+sudo dpkg -i ./libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+rm libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+
 curl -kfsSL --retry 3 https://aka.ms/vsls-linux-prereq-script -o ~/vsls-reqs
 chmod +x ~/vsls-reqs
 ~/vsls-reqs
 rm ~/vsls-reqs
 
-# ubuntu 22.04 don't have libssl 1.1
-# curl -kfsSL --retry 3 -o http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-# sudo dpkg -i ./libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-# rm libssl1.1_1.1.0g-2ubuntu4_amd64.deb
