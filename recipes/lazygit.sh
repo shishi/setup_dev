ce1d94b49653a91ba615e75cab5aafa7d3d3d23a
Shigenobu Nishikawa <shishi@srevo.net>
2023-01-06 19:27:17 +0900

    lazygit

--- recipes/lazygit.sh
+++ recipes/lazygit.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/bash
 
 set -ex
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-35.]+')
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"v*([^"]+)".*/\1/')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+
 tar xf lazygit.tar.gz -C ~/.local/bin lazygit
 chmod +x ~/.local/bin/lazygit
 rm -f lazygit.tar.gz
