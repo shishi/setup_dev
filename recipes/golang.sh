8c0bd9d0ce78f6907308dca3a3adb1a70d30d6ec
Shigenobu Nishikawa <shishi@srevo.net>
2023-01-24 00:43:30 +0900

    make sure go env

--- recipes/golang.sh
+++ recipes/golang.sh
@@ -7,7 +7,10 @@ sudo apt-get update -qq
 sudo apt-get install -yqq --no-install-recommends golang
 
 mkdir -p ~/.local/bin
-GO_BIN=~/.local/bin go install github.com/junegunn/fzf@latest
-GO_BIN=~/.local/bin go install github.com/x-motemen/ghq@latest
-GO_BIN=~/.local/bin go install github.com/rhysd/actionlint/cmd/actionlint@latest
-GO_BIN=~/.local/bin go install github.com/lighttiger2505/sqls@latest
+export GO_BIN=~/.local/bin
+export GO_PATH=~/dev
+
+go install github.com/junegunn/fzf@latest
+go install github.com/x-motemen/ghq@latest
+go install github.com/rhysd/actionlint/cmd/actionlint@latest
+go install github.com/lighttiger2505/sqls@latest
