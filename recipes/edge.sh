464682b55619d2e09508966402d8c715a44a975c
Shigenobu Nishikawa <shishi@srevo.net>
2023-02-09 14:38:04 +0900

    typo

--- recipes/edge.sh
+++ recipes/edge.sh
@@ -3,7 +3,7 @@
 set -ex
 
 curl -fsS --retry 3 https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/edge.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/edge.gpg] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/edge.list
+echo 'deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/edge.gpg] https://packages.microsoft.com/repos/edge stable main' | sudo tee /etc/apt/sources.list.d/edge.list
 
 sudo apt-get update -qq
-sudo apt-get install -yqq microsoft-edge-stable
+sudo apt-get install -yqq --no-install-recommends microsoft-edge-stable
--- recipes/vscode.sh
+++ recipes/vscode.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-curl -fsS --retry 3 https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --batch --yes --dearmor -o > /etc/apt/keyrings/vscode.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/vscode.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+curl -fsS --retry 3 https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/vscode.gpg
+echo 'deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/vscode.gpg] https://packages.microsoft.com/repos/code stable main' | sudo tee /etc/apt/sources.list.d/vscode.list
 
 sudo apt-get update -qq
-sudo apt-get install -yqq code apt-transport-https
+sudo apt-get install -yqq --no-install-recommends code apt-transport-https
