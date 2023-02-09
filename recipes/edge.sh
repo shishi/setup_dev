3cc528b2c2ac815166da98732c5f5772ee0ab0c6
Shigenobu Nishikawa <shishi@srevo.net>
2023-02-09 14:34:05 +0900

    fix repo sign

--- recipes/edge.sh
+++ recipes/edge.sh
@@ -2,10 +2,8 @@
 
 set -ex
 
-curl -fsS --retry 3 https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/edge.list'
-rm -f packages.microsoft.gpg
+curl -fsS --retry 3 https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/edge.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/edge.gpg] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/edge.list
 
 sudo apt-get update -qq
 sudo apt-get install -yqq microsoft-edge-stable
--- recipes/vscode.sh
+++ recipes/vscode.sh
@@ -2,10 +2,8 @@
 
 set -ex
 
-curl -fsS --retry 3 https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
+curl -fsS --retry 3 https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --batch --yes --dearmor -o > /etc/apt/keyrings/vscode.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/vscode.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 
 sudo apt-get update -qq
 sudo apt-get install -yqq code apt-transport-https
