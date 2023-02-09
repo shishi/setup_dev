09a0fb71caa32a0eb02fd8b62eedf49eb9878308
Shigenobu Nishikawa <shishi@srevo.net>
2023-02-09 14:06:32 +0900

    use tee

--- recipes/vivaldi.sh
+++ recipes/vivaldi.sh
@@ -3,6 +3,6 @@
 set -ex
 
 curl -fsS --retry 3 https://repo.vivaldi.com/archive/linux_signing_key.pub  | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/vivaldi.gpg
-echo 'deb https://repo.vivaldi.com/archive/deb/ stable main' >> /etc/apt/sources.list.d/vivaldi.list
+echo 'deb https://repo.vivaldi.com/archive/deb/ stable main' | sudo tee /etc/apt/sources.list.d/vivaldi.list
 sudo apt-get update -qq
 sudo apt-get install -yqq --no-install-recommends vivaldi-stable
