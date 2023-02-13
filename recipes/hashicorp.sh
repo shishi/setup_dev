4da7a1c388f543215d316acaed79cccefe976c5d
Shigenobu Nishikawa <shishi@srevo.net>
2023-02-14 00:12:37 +0900

    virtualbox-qt

--- recipes/hashicorp.sh
+++ recipes/hashicorp.sh
@@ -6,4 +6,4 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /us
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 sudo apt-get update -qq
-sudo apt-get install -yqq --no-install-recommends vagrant virtualbox
+sudo apt-get install -yqq --no-install-recommends vagrant virtualbox virtualbox-qt
