9e7f8e20d293557882bda4126c8018f100d221cf
Shigenobu Nishikawa <shishi@srevo.net>
2023-03-10 17:12:54 +0900

    add docker buildx plugin

--- recipes/docker.sh
+++ recipes/docker.sh
@@ -6,10 +6,10 @@ sudo apt-get install -yqq --no-install-recommends ca-certificates curl gnupg lsb
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -qq
-sudo apt-get install -yqq --no-install-recommends docker-ce docker-ce-cli containerd.io docker-compose-plugin golang-docker-credential-helpers
+sudo apt-get install -yqq --no-install-recommends docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-buildx-plugin golang-docker-credential-helpers
 
 sudo usermod -aG docker ${USER}
 sudo systemctl enable --now docker
