0189035e7a4b7fac02ed40d81f718a4f5e8743cd
Shigenobu Nishikawa <shishi@srevo.net>
2023-09-01 17:51:04 +0900

    add protolint

--- recipes/golang.sh
+++ recipes/golang.sh
@@ -10,10 +10,10 @@ mkdir -p ${HOME}/.local/bin
 export GOBIN=${HOME}/.local/bin
 
 go install github.com/junegunn/fzf@latest
-go install github.com/x-motemen/ghq@latest
-go install github.com/rhysd/actionlint/cmd/actionlint@latest
 go install github.com/lighttiger2505/sqls@latest
-go install github.com/bufbuild/buf/cmd/buf@latest
-go install golang.org/x/tools/gopls@latest
+go install github.com/rhysd/actionlint/cmd/actionlint@latest
+go install github.com/x-motemen/ghq@latest
+go install github.com/yoheimuta/protolint/cmd/protolint@latest
 go install golang.org/x/tools/cmd/goimports@latest
+go install golang.org/x/tools/gopls@latest
 # go install github.com/lemonade-command/lemonade@latest
