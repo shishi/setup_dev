2a946afa8f7547a26af4911ce3c7d2d874cf02e9
Shigenobu Nishikawa <shishi@srevo.net>
2023-09-01 17:52:51 +0900

    add protoc

--- recipes/basic_packages.sh
+++ recipes/basic_packages.sh
@@ -3,7 +3,7 @@
 set -e
 
 packages=("build-essential" "pkg-config" "software-properties-common" "python-is-python3" "python3-venv" "luajit" "luarocks" "gpg" "pass" "xsel")
-packages+=("git" "curl" "wget" "unzip" "silversearcher-ag" "ripgrep" "vim" "rclone" "jq" "tidy")
+packages+=("git" "curl" "wget" "unzip" "silversearcher-ag" "ripgrep" "vim" "rclone" "jq" "tidy" "protobuf-compiler")
 packages+=("mysql-client" "libmysqlclient-dev" "postgresql-client" "libpq-dev" "sqlite3" "libsqlite3-dev" "redis-tools")
 packages+=("imagemagick" "libmagickwand-dev")
 packages+=("fonts-noto" "fonts-noto-extra" "fonts-noto-mono" "fonts-noto-cjk" "fonts-noto-cjk-extra" "fonts-noto-ui-core" "fonts-noto-ui-extra" "fonts-noto-color-emoji" "fonts-ibm-plex")
