95ab69a000ab8f35974fb6dfbab9a09e57aa11d1
Shigenobu Nishikawa <shishi@srevo.net>
2023-09-18 21:26:31 +0900

    unar

--- recipes/basic_packages.sh
+++ recipes/basic_packages.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-packages=("build-essential" "pkg-config" "software-properties-common" "python-is-python3" "python3-venv" "luajit" "luarocks" "gpg" "pass" "xsel")
-packages+=("git" "curl" "wget" "unzip" "silversearcher-ag" "ripgrep" "vim" "rclone" "jq" "tidy" "protobuf-compiler")
+packages=("build-essential" "pkg-config" "software-properties-common" "python-is-python3" "python3-venv" "luajit" "luarocks" "gpg")
+packages+=("pass" "xsel")
+packages+=("git" "curl" "wget" "unzip" "unar" "silversearcher-ag" "vim" "rclone" "jq" "tidy" "protobuf-compiler" "poppler-utils" "poppler-data")
 packages+=("mysql-client" "libmysqlclient-dev" "postgresql-client" "libpq-dev" "sqlite3" "libsqlite3-dev" "redis-tools")
 packages+=("imagemagick" "libmagickwand-dev")
 packages+=("fonts-noto" "fonts-noto-extra" "fonts-noto-mono" "fonts-noto-cjk" "fonts-noto-cjk-extra" "fonts-noto-ui-core" "fonts-noto-ui-extra" "fonts-noto-color-emoji" "fonts-ibm-plex")
--- recipes/rustup.sh
+++ recipes/rustup.sh
@@ -8,4 +8,4 @@ mkdir -p ~/.cargo
 cp ./files/cargo_config ~/.cargo/config
 
 ~/.cargo/bin/rustup component add rust-src rust-analyzer rustfmt clippy
-~/.cargo/bin/cargo install cargo-edit bat eza fd-find selene stylua taplo-cli tree-sitter-cli
+~/.cargo/bin/cargo install cargo-edit bat eza fd-find ripgrep selene stylua taplo-cli tree-sitter-cli
