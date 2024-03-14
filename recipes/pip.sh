a53fa5e6c7a414f26a42c3303f18385252df8003
Shigenobu Nishikawa <shishi@srevo.net>
2024-03-14 23:02:05 +0900

    needs linters

--- recipes/pip.sh
+++ recipes/pip.sh
@@ -6,4 +6,4 @@ curl -fsSL --retry 3 https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py --user
 rm get-pip.py
 
-~/.local/bin/pip install awscli sqlfluff yamllint beautysh
+~/.local/bin/pip install awscli sqlfluff yamllint 
