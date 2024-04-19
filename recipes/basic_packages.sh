d99f4c87cb8c6c14e20fcb3417d83403576d75a2
Shigenobu Nishikawa <shishi@srevo.net>
2024-04-19 09:32:15 +0900

    add clang-format for formatting proto

--- recipes/basic_packages.sh
+++ recipes/basic_packages.sh
@@ -4,7 +4,7 @@ set -e
 
 packages=("build-essential" "pkg-config" "software-properties-common" "python-is-python3" "python3-venv" "luajit" "luarocks" "gpg")
 packages+=("pass" "xsel")
-packages+=("git" "curl" "wget" "unzip" "unar" "silversearcher-ag" "vim" "rclone" "jq" "tidy" "protobuf-compiler" "poppler-utils" "poppler-data")
+packages+=("git" "curl" "wget" "unzip" "unar" "silversearcher-ag" "vim" "rclone" "jq" "tidy" "protobuf-compiler" "poppler-utils" "poppler-data" "clang-format")
 packages+=("mysql-client" "libmysqlclient-dev" "postgresql-client" "libpq-dev" "sqlite3" "libsqlite3-dev" "redis-tools")
 packages+=("imagemagick" "libmagickwand-dev")
 packages+=("fonts-noto" "fonts-noto-extra" "fonts-noto-mono" "fonts-noto-cjk" "fonts-noto-cjk-extra" "fonts-noto-ui-core" "fonts-noto-ui-extra" "fonts-noto-color-emoji" "fonts-ibm-plex")
