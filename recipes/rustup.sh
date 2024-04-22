6c4f4f705b2b1c8012f8bf7c5e4557611d6427d4
Shigenobu Nishikawa <shishi@srevo.net>
2024-04-22 22:04:33 +0900

    taplo

--- recipes/rustup.sh
+++ recipes/rustup.sh
@@ -8,4 +8,5 @@ mkdir -p ~/.cargo
 cp ./files/cargo_config ~/.cargo/config
 
 ~/.cargo/bin/rustup component add rust-src rust-analyzer rustfmt clippy
-~/.cargo/bin/cargo install cargo-edit bat eza fd-find ripgrep selene stylua taplo-cli tree-sitter-cli
+~/.cargo/bin/cargo install cargo-edit bat eza fd-find ripgrep selene stylua tree-sitter-cli
+# ~/.cargo/bin/cargo install --all-features --locked taplo-cli
