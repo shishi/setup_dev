18d29e07aace8e42d48639ebc6eac533556d4c8e
Shigenobu Nishikawa <shishi@srevo.net>
2024-05-09 10:31:49 +0900

    biome

--- recipes/nodejs.sh
+++ recipes/nodejs.sh
@@ -6,4 +6,4 @@ sudo curl -fsSL --retry 3 https://deb.nodesource.com/setup_lts.x | sudo bash -
 sudo apt-get update -qq
 sudo apt-get install -yqq --no-install-recommends nodejs
 
-npm --prefix ~/.local/ install -g typescript prettier eslint eslint-config-prettier eslint-plugin-react eslint-plugin-react-hooks stylelint stylelint-config-standard sql-formatter @vtsls/language-server markdownlint markuplint buf
+npm --prefix ~/.local/ --save-dev install -g typescript prettier eslint eslint-config-prettier eslint-plugin-react eslint-plugin-react-hooks stylelint stylelint-config-standard sql-formatter @vtsls/language-server markdownlint markuplint buf @biomejs/biome
